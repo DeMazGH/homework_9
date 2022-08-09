b82097e311e12f979c22422dd09794502e64f1a9
Denis <demaz@bk.ru>
2022-08-09 11:14:20 +0700

    homework_9 done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Book muMu = new Book("Mu-Mu", "Ivan Turgenev", 2000);
+        Book dogHeart = new Book("Dog's heart", "Mikhail Bulgakov", 2011);
+        Author bulgakov = new Author("Mikhail", "Bulgakov");
         Author turgenev = new Author("Ivan", "Turgenev");
 
-        System.out.println("turgenev.getFirstName() = " + turgenev.getFirstName());
-        System.out.println("turgenev.getLastName() = " + turgenev.getLastName());
+        Printer.printBookProperties(muMu);
 
-        System.out.println("muMu.getName() = " + muMu.getName());
-        System.out.println("muMu.getAuthor() = " + muMu.getAuthor());
-        System.out.println("muMu.getPublicationYear() = " + muMu.getPublicationYear());
         muMu.setPublicationYear(2022);
         System.out.println("muMu.getPublicationYear() = " + muMu.getPublicationYear());
+        System.out.println();
+
+        Printer.printBookProperties(dogHeart);
+
+        Printer.printAuthorProperties(turgenev);
+
+        Printer.printAuthorProperties(bulgakov);
     }
 
 }
---
+++ src/Printer.java
@@ -0,0 +1,17 @@
+public class Printer {
+
+    public static void printBookProperties(Book book) {
+        System.out.println("Свойства книги " + book.getName());
+        System.out.println(".getName() = " + book.getName());
+        System.out.println(".getAuthor() = " + book.getAuthor());
+        System.out.println(".getPublicationYear() = " + book.getPublicationYear());
+        System.out.println();
+    }
+
+    public static void printAuthorProperties(Author author) {
+        System.out.println("Данные автора " + author.getFirstName() + " " + author.getLastName());
+        System.out.println(".getFirstName() = " + author.getFirstName());
+        System.out.println(".getLastName() = " + author.getLastName());
+        System.out.println();
+    }
+}
